f7f559d1b93684adb0899ad8c7e1bfc540b2c421
ECE496TutorialBot <61518820+ECE496TutorialBot@users.noreply.github.com>
2020-02-26 15:32:41 -0500

    identity function

--- code.py
+++ code.py
@@ -1,9 +1,5 @@
 def add(a,b):
     return a+b
 
-def no_op():
-    '''
-        Write a meaningful function here, unlike the one above
-
-        Then, add a test case for this function in tests/test.py'''
-    return
+def id(a):
+    return a
